48ec7237d6e4e17d29cc8167448bba4d1a59637b
lionoa <root@lionoa.net>
2020-08-09 14:36:40 +0800

    增加代码间的空行

--- src/ClassTest/Test1.java
+++ src/ClassTest/Test1.java
@@ -15,40 +15,52 @@ public class Test1 {
     }
 }
 class Animal{
+
     private String name;
     private int id;
+
     public Animal(String myName, int myId) {
         name = myName;
         id = myId;
     }
+
     public void eat(){
         System.out.println(name+"正在吃");
     }
+
     public void sleep(){
         System.out.println(name+"正在睡");
     }
+
     public void introduction() {
         System.out.println("大家好！我是"         + id + "号" + name + ".");
     }
 }
+
 class Mouse extends Animal{
+
     public Mouse(String myName, int myId){
         super(myName,myId);
     }
+
     public void play(){
         System.out.println("i am playing basketball");
         super.eat();
 
     }
+
     public void display(){
         super.introduction();
         this.play();
     }
 }
+
 class smallMouse extends Mouse{
+
     public smallMouse(String myName, int myId){
         super(myName, myId);
     }
+
     public void display(){
         System.out.println("hello world!");
     }
